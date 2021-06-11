d9efe43b9020706970902fbb278f8043e48df2be
Nguyen Duc Duy <duynd1@tinhvan.com>
2021-06-11 17:33:43 +0700

    dependencies update

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,8 +1,46 @@
-import {Controller} from '@nestjs/common';
+import {Controller, Get, Param, Post, Req} from '@nestjs/common';
+import {Request} from 'express';
 
 @Controller('api/v2')
 export class AppController {
 
+    @Get('service')
+    public getServiceInfo(@Req() request: Request) {
 
+    }
+
+    @Get('networks')
+    public listNetworks(@Req() request: Request) {
+
+    }
+
+    @Get('network/:networkid/state')
+    public getNetworkState(@Param('networkid') networkId: number, @Req() request: Request) {
+
+    }
+
+    @Get('network/:networkid/members')
+    public getNetworkMembers(@Param('networkid') networkId: number, @Req() request: Request) {
+
+    }
+
+    @Get('members')
+    public getAllMembers(@Req() request: Request) {
+
+    }
+
+    @Get('member/:memberid/devices')
+    public getMemberDevices(@Param('memberid') memberId: string, @Req() request: Request) {
+    }
+
+    @Post('member/remove')
+    public removeMemberFromNetwork(@Req() request: Request) {
+
+    }
+
+    @Post('member/add')
+    public addMemberToNetwork(@Req() request: Request) {
+
+    }
 
 }
